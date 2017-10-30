989c7cac18f03284ac7d1939272c4382cf58a388
davet2408 <davet2408@googlemail.com>
2017-10-30 12:17:21 +0000

    Finished game class and main class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-/**
- * Created by k1763508 on 30/10/17.
- */
+
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello world");
+        new Game().run();
     }
 }
